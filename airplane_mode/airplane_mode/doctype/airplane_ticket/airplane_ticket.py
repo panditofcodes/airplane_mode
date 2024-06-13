c50025a0e1c5d0718612807da7a0599f5a4f3877
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2024-06-13 19:04:49 +0530

    Exercise of day 2

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -2,12 +2,17 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
+import random, string
 
 
-class AirplaneTicket(Document):
+class AirplaneTicket(WebsiteGenerator):
 	def before_save(self):
-		self.set_total_amount()
 
-	def set_total_amount(self):
-		pass
+		self.seat_no = f'{random.randint(1,self.capicity)}{random.choice(string.ascii_lowercase)}'
+
+		total_add_ons_price = 0 # to store total add ons price
+		for items in self.add_ons:   # this loop will calculate add ons price
+			total_add_ons_price += items.amount
+
+		self.final_amount = self.fare + total_add_ons_price  # here we are setting total fare of the fligt
